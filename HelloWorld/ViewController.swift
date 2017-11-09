3a914aed096ffb22e0df3a802a7fa8e8c7f77c40
XIAODONG Dai <xiaodongdai@xiaodongs-mbp.ki.sw.ericsson.se>
2017-11-09 14:29:45 +0100

    Initial Commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by XIAODONG Dai on 2017-11-09.
+//  Copyright © 2017 XIAODONG Dai. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
